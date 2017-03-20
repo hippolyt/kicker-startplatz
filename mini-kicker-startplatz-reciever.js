3421b4777f2e52fb6e7bbc16e48ef855f78976eb
Sven Wildermann <git@wildermannonline.de>
2017-03-20 18:17:18 +0100

    final state after IoT Makerthon

--- mini-kicker-startplatz-reciever.js
+++ mini-kicker-startplatz-reciever.js
@@ -1,4 +1,6 @@
 let timer = 0
+let kick_change = 0
+let value = 0
 basic.forever(() => {
     while (timer > 0) {
         basic.pause(1000)
@@ -12,22 +14,29 @@ basic.forever(() => {
                 # . . . .
                 # . . . .
                 `)
+            if (kick_change == 1 && value == 0) {
+                serial.writeLine("Frei")
+                kick_change = 0
+            }
         } else {
             basic.setLedColor(Colors.Red)
         }
     }
 })
-input.onButtonPressed(Button.A, () => {
-    basic.showNumber(5)
-    basic.setLedColor(Colors.Blue)
-})
 input.onButtonPressed(Button.B, () => {
     basic.clearScreen()
     basic.setLedColor(Colors.Yellow)
 })
+input.onButtonPressed(Button.A, () => {
+    basic.showNumber(5)
+    basic.setLedColor(Colors.Blue)
+})
 radio.onDataPacketReceived(({receivedString: kicker, receivedNumber: value}) => {
     music.playTone(Note.C, music.beat(BeatFraction.Whole))
     if (value == 1) {
+        if (kick_change == 0) {
+            kick_change = 1
+        }
         music.playTone(Note.C, music.beat(BeatFraction.Whole))
         timer = 30000
         basic.setLedColor(Colors.Red)
@@ -39,8 +48,15 @@ radio.onDataPacketReceived(({receivedString: kicker, receivedNumber: value}) => 
             . # . # .
             # . . . #
             `)
+        if (kick_change == 1 && value == 1) {
+            serial.writeLine("besetzt")
+            kick_change = 0
+        }
     }
     if (value == 2) {
+        if (kick_change == 0) {
+            kick_change = 1
+        }
         radio.sendValue("kicker", 5)
         music.playTone(Note.C, music.beat(BeatFraction.Half))
         basic.showString("SPIELER GESUCHT")
@@ -51,8 +67,15 @@ radio.onDataPacketReceived(({receivedString: kicker, receivedNumber: value}) => 
             . # . . .
             . . . . .
             `)
+        if (kick_change == 1 && value == 2) {
+            serial.writeLine("gesucht")
+            kick_change = 0
+        }
     }
     if (value == 3) {
+        if (kick_change == 0) {
+            kick_change = 1
+        }
         radio.sendValue("kicker", 6)
         music.playTone(Note.C, music.beat(BeatFraction.Quarter))
         basic.showString("Turnament")
@@ -65,7 +88,12 @@ radio.onDataPacketReceived(({receivedString: kicker, receivedNumber: value}) => 
             `)
     }
 })
+serial.redirect(
+    SerialPin.C16,
+    SerialPin.C17,
+    BaudRate.BaudRate9600
+)
+kick_change = 0
 radio.setGroup(6)
 timer = 0
 music.playTone(Note.B, music.beat(BeatFraction.Eighth))
-
